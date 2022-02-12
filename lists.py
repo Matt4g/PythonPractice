73e6fab2efb6e2f9b5b7f2d74dab6048a9d54a8e
Matt <mglad776@gmail.com>
2022-02-12 13:24:01 -0500

    Easy commits

---
+++ lists.py
@@ -0,0 +1,15 @@
+# Lists: ordered, mutable, allows duplicate elements
+from operator import itemgetter
+
+
+my_list = ['banana', 'cherry', 'apple']
+print(my_list)
+
+#my_list2 = [5, True, "apple", "apple"]
+#print(my_list2)
+ 
+# #item = my_list[0]
+#print(item)
+
+for i in my_list:
+    print(i)
