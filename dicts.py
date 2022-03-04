4326d170a9dfb42660b769d80331318c07e49a41
Matt <mglad776@gmail.com>
2022-03-04 12:52:12 -0500

    Added Dictionaries/WIP Still

---
+++ dicts.py
@@ -0,0 +1,60 @@
+# Dictionary: Key-Value paris, Unordered, Mutable
+from ast import If
+from cgi import print_form
+from optparse import Values
+from types import CoroutineType
+from unicodedata import name
+
+
+mydict = {"name": "Max", "age": 28, 'city': 'New York'}
+
+#mydict2 = dict(name = 'Tim', age = 20, city = 'Boston')
+#print(mydict2)
+
+#value = mydict['name']
+#print(value)
+
+#
+#mydict['email'] = 'max@xyz.com'
+#del mydict['email']
+#mydict.pop('email')
+#mydict.popitem()
+
+#Test if a key is in dictionary
+#if 'name' in mydict:
+   # print(mydict['name'])
+
+
+#try:#
+#  print(mydict["name"])
+#except:#
+#    print("Error")
+    
+#try:
+#    print(mydict["lastname"])
+#except:
+#   print("Error")
+
+#Print out all the keys and items in the dictionary
+#for key in mydict.keys():
+##    print(key)
+#for value in mydict.values():
+#    print(value)
+#for key, value in mydict.items():
+#   print(key, value)
+
+#Copy Dictionaries
+#mydict_cpy = mydict.copy()
+#mydict_cpy = dict(mydict)
+
+#mydict_cpy['email'] = 'max@xyz.com'
+#print(mydict_cpy)
+
+#Combine dictionaries
+#my_dict = {'name': ';Max', 'age': '28', 'email': 'max@xyz.com'}
+#my_dict_2 = dict(name = 'Mary', age = '27', city = 'Boston')
+
+#my_dict.update(my_dict_2)
+#print(my_dict)
+
+print(mydict)
